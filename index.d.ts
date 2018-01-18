73b9c676024a3014f0f59f8ea4086af64d8119ec
hyurl <i@hyurl.com>
2018-01-18 12:28:55 +0800

    Add TypeScript declaration file.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 import * as OutputBuffer from "sfn-output-buffer";
 import * as Mail from "sfn-mail";
 
-export declare class Logger extends OutputBuffer {
+declare class Logger extends OutputBuffer {
     constructor(optinos: {
         ttl?: number,
         size?: number,
@@ -25,3 +25,5 @@ export declare class Logger extends OutputBuffer {
     /** Outputs a message to the log file at ERROR level. */
     error(...msg: string[]): void;
 }
+
+export = Logger;
